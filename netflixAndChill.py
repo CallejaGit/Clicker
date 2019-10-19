06a5e4515c01f0af9591c8db325d27a1d29c4397
Patrick <patrickcalleja@protonmail.com>
2019-10-19 14:23:18 -0400

    to prompt user to add time units respectively

--- netflixAndChill.py
+++ netflixAndChill.py
@@ -2,7 +2,11 @@ import pyautogui
 import subprocess
 import time
 import os
+from datetime import date
+from datetime import datetime
 
+
+# poor man's loading UI
 def waiting(tick):
     if tick == 0:
         os.system('clear')
@@ -17,8 +21,20 @@ def waiting(tick):
         os.system('clear')
         print("waiting...")
 
+# makesure it will click on the centre of the screen
 output = subprocess.Popen('xrandr | grep "\*" | cut -d" " -f4',shell=True, stdout=subprocess.PIPE).communicate()[0]
 x_axis, y_axis = "", ""
+
+
+# Prompts user when the event occurs
+user_input_hh = raw_input("Enter hour in 24h format ##: ")
+user_input_mm = raw_input("Enter minutes in 24h format ##: ")
+user_input_ss = raw_input("Enter seconds 24h format ##: ")
+
+# Set up to conver EST > Unix Epoch
+set_time = date.today().strftime("%d/%m/%Y") + " " + user_input_hh + \
+        ":" + user_input_mm + ":" + user_input_ss
+unix_epoch = time.mktime(datetime.strptime(set_time, "%d/%m/%Y %H:%M:%S").timetuple())
 
 # poor man's regex
 by = False;
@@ -37,14 +53,10 @@ for s in output:
 x_axis = int(x_axis) / 2
 y_axis = int(y_axis) / 2
 
-num = int(input("Enter Unix Epoch: "))
-
 # poor man's scheduler
 tick = 0;
-while time.time() < num:
-    # do nothing
-
+while time.time() < unix_epoch:
     waiting(tick)
     tick = (tick + 1) % 5000000
+print("enjoy")
 pyautogui.click(x_axis, y_axis)
-print time.time()
